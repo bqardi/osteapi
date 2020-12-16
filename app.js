e0066d61b8f1dba72d02e4aaef373f47c5ffa94a
bqardi <51706945+bqardi@users.noreply.github.com>
2020-12-16 09:06:22 +0100

    CORS added

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 var express = require("express");
 var app = express();
 var formidable = require("express-formidable");
+var cors = require("cors");
 
 require("./database");
+app.use(cors()); //CORS manual for KUN at tillade tilgang fra egen webside
 app.use(formidable());
 
 require("./cheeses.route")(app);
